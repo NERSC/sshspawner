4db2857f6dd7b5ddd382a38ab54b115420caf074
Shreyas Cholia <shreyas@gmail.com>
2016-08-22 14:30:16 -0700

    Sample NERSC config

---
+++ jupyterhub_config.py
@@ -0,0 +1,11 @@
+
+c.JupyterHub.spawner_class = 'sshspawner.SSHSpawner'
+
+c.SSHSpawner.remote_host = 'cori19-224.nersc.gov'
+c.SSHSpawner.remote_port = '2222'
+c.SSHSpawner.ssh_command = 'gsissh'
+
+c.SSHSpawner.use_gsi = True
+c.SSHSpawner.path = '/global/common/cori/software/python/3.5-anaconda/bin:/global/common/cori/das/jupyterhub/:/usr/common/usg/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games:/usr/lib/mit/bin:/usr/lib/mit/sbin'
+c.SSHSpawner.remote_port_command = '/global/common/cori/das/jupyterhub/get_port.py'
+
