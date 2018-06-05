6787fd9cd8d19c0866e1fd03c191ba6c82621830
William Krinsman <williamkrinsman@lbl.gov>
2018-06-05 09:42:56 -0700

    Testing removing the explicit timeout

--- sshspawner/sshspawner.py
+++ sshspawner/sshspawner.py
@@ -143,15 +143,16 @@ class SSHSpawner(Spawner):
                                                     stdout=asyncio.subprocess.PIPE, 
                                                     stderr=asyncio.subprocess.PIPE,
                                                     env=ssh_env)
-        try:
+#        try:
             # changed the timeout to 300 temporarily for testing purposes (and because that's BatchSpawner's default)
-            stdout, stderr = await asyncio.wait_for(proc.communicate(), timeout=300)
-        except asyncio.TimeoutError:
-            self.log.debug("execute timed out")
-            proc.kill()
-            self.log.debug("execute timed out done kill")
-            stdout, stderr = await proc.communicate()
-            self.log.debug("execute timed out done communicate")
+#            stdout, stderr = await asyncio.wait_for(proc.communicate(), timeout=300)
+        stdout, stderr = await proc.communicate()
+#        except asyncio.TimeoutError:
+#            self.log.debug("execute timed out")
+#            proc.kill()
+#            self.log.debug("execute timed out done kill")
+#            stdout, stderr = await proc.communicate()
+#            self.log.debug("execute timed out done communicate")
 
         returncode = proc.returncode
 
