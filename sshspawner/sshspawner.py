1d5c100764a2dbf067121fe596a2b8a9eca3927f
Rollin Thomas <rcthomas@lbl.gov>
2018-06-29 10:43:32 -0700

    Flow more like other spawners, addt'l FIXMEs

--- sshspawner/sshspawner.py
+++ sshspawner/sshspawner.py
@@ -82,33 +82,227 @@ class SSHSpawner(Spawner):
     # FIXME this should be a traitlet probably?
     pid = None
 
+    # TODO When we add host pool, we need to keep host/ip too, not just PID.
+    def load_state(self, state):
+        """Restore state about ssh-spawned server after a hub restart.
+
+        The ssh-spawned processes only need the process id."""
+        super().load_state(state)
+        if "pid" in state:
+            self.pid = state["pid"]
+
+    # TODO When we add host pool, we need to keep host/ip too, not just PID.
+    def get_state(self):
+        """Save state needed to restore this spawner instance after hub restore.
+
+        The ssh-spawned processes only need the process id."""
+        state = super().get_state()
+        if self.pid:
+            state["pid"] = self.pid
+        return state
+
+    # TODO When we add host pool, we need to clear host/ip too, not just PID.
+    def clear_state(self):
+        """Clear stored state about this spawner (pid)"""
+        super().clear_state()
+        self.pid = 0
+
+    # FIXME this looks like it's done differently now, there is get_env which
+    # actually calls this.
+    def user_env(self):
+        """Augment env of spawned process with user-specific env variables."""
+
+        # FIXME I think the JPY_ variables have been deprecated in JupyterHub
+        # since 0.7.2, we should replace them.  Can we figure this out?
+
+        env = super(SSHSpawner, self).get_env()
+        env.update(dict(
+            JPY_USER=self.user.name,
+            JPY_COOKIE_NAME=self.user.server.cookie_name,
+            JPY_BASE_URL=self.user.server.base_url,
+            JPY_HUB_PREFIX=self.hub.server.base_url,
+            JUPYTERHUB_PREFIX=self.hub.server.base_url,
+            PATH=self.path
+        ))
+
+        if self.notebook_dir:
+            env['NOTEBOOK_DIR'] = self.notebook_dir
+
+        hub_api_url = self.hub.api_url
+        if self.hub_api_url != '':
+            hub_api_url = self.hub_api_url
+
+        env['JPY_HUB_API_URL'] = hub_api_url
+        env['JUPYTERHUB_API_URL'] = hub_api_url
+
+        return env
+
+    async def start(self):
+        """Start single-user server on remote host."""
+
+        port = await self.remote_random_port()
+        if port is None or port == 0:
+            return False
+        cmd = []
+
+        cmd.extend(self.cmd)
+        cmd.extend(self.get_args())
+
+        if self.hub_api_url != "":
+            old = "--hub-api-url={}".format(self.hub.api_url)
+            new = "--hub-api-url={}".format(self.hub_api_url)
+            for index, value in enumerate(cmd):
+                if value == old:
+                    cmd[index] = new
+        for index, value in enumerate(cmd):
+            if value[0:6] == '--port':
+                cmd[index] = '--port=%d' % (port)
+
+        remote_cmd = ' '.join(cmd)
+
+        # time.sleep(2)
+        # import pdb; pdb.set_trace()
+
+        self.pid = await self.exec_notebook(remote_cmd)
+
+        self.log.debug("Starting User: {}, PID: {}".format(self.user.name, self.pid))
+
+        if self.pid < 0:
+            return None
+        # DEPRECATION: Spawner.start should return a url or (ip, port) tuple in JupyterHub >= 0.9
+        return (self.remote_host, port)
+
+    async def poll(self):
+        """Poll ssh-spawned process to see if it is still running.
+
+        If it is still running return None. If it is not running return exit
+        code of the process if we have access to it, or 0 otherwise."""
+
+        if not self.pid:
+                # no pid, not running
+            self.clear_state()
+            return 0
+
+        # send signal 0 to check if PID exists
+        alive = await self.remote_signal(0)
+        self.log.debug("Polling returned {}".format(alive))
+
+        if not alive:
+            self.clear_state()
+            return 0
+        else:
+            return None
+
+    # FIXME handle the now business like the documentation actually says
+    async def stop(self, now=False):
+        """Stop single-user server process for the current user.
+
+        If `now` is False (default), shutdown the server as gracefully as possible,
+        e.g. starting with SIGINT, then SIGTERM, then SIGKILL.
+        If `now` is True, terminate the server immediately.
+        The coroutine should return when the process is no longer running."""
+        alive = await self.remote_signal(15)
+        self.clear_state()
+
     def get_remote_user(self, username):
-        """
-        Maps a jupyterhub username to a remote user. Override this if
-        you need to return a different name
-        """
+        """Map JupyterHub username to remote username."""
         return username
 
     def get_gsi_cert(self):
-        """
-        Returns location of x509 user cert. Override this if you need to
-        return a different path (Deprecated)
-        """
+        """Get location of x509 user cert. (Deprecated)"""
         return self.gsi_cert_path.replace("%U", self.user.name)
 
     def get_gsi_key(self):
-        """
-        Returns location of x509 user key. Override this if you need to
-        return a different path (Deprecated)
-        """
+        """Get location of x509 user key. (Deprecated)"""
         return self.gsi_key_path.replace("%U", self.user.name)
 
+    async def remote_random_port(self):
+        """Select unoccupied port on the remote host and return it. 
+        
+        If this fails for some reason return `None`."""
+
+        # FIXME this keeps getting repeated
+        # pass this into bash -c 'command'
+        # command needs to be in "" quotes, with all the redirection outside
+        # eg. bash -c '"ls -la" < /dev/null >> out.txt'
+
+        command = '"{}" < /dev/null'.format(self.remote_port_command)
+
+        stdout, stderr, retcode = await self.execute(command)
+
+        if stdout != b"":
+            port = int(stdout)
+            self.log.debug("port={}".format(port))
+        else:
+            port = None
+            self.log.error("Failed to get a remote port")
+        return port
+
+    # FIXME add docstring
+    async def exec_notebook(self, command):
+        """TBD"""
+
+        env = self.user_env()
+        bash_script_str = "#!/bin/bash\n"
+
+        for item in env.items():
+            # item is a (key, value) tuple
+            # command = ('export %s=%s;' % item) + command
+            bash_script_str += 'export %s=%s\n' % item
+
+        # FIXME this keeps getting repeated
+        # pass this into bash -c 'command'
+        # command needs to be in "" quotes, with all the redirection outside
+        # eg. bash -c '"ls -la" < /dev/null >> out.txt'
+
+        # We pass in /dev/null to stdin to avoid the hang
+        # Finally Grab the PID
+        # command = '"%s" < /dev/null >> jupyter.log 2>&1 & pid=$!; echo $pid' % command
+
+        bash_script_str += '%s < /dev/null >> jupyter.log 2>&1 & pid=$!\n' % command
+        bash_script_str += 'echo $pid\n'
+
+        run_script = "/tmp/{}_run.sh".format(self.user.name)
+        with open(run_script, "w") as f:
+            f.write(bash_script_str)
+        if not os.path.isfile(run_script):
+            raise Exception("The file " + run_script + "was not created.")
+        else:
+            with open(run_script, "r") as f:
+                self.log.debug(run_script + " was written as:\n" + f.read())
+
+        stdout, stderr, retcode = await self.execute(command, stdin=run_script)
+        self.log.debug("exec_notebook status={}".format(retcode))
+        if stdout != b'':
+            pid = int(stdout)
+        else:
+            return -1
+
+        return pid
+
+    async def remote_signal(self, sig):
+        """Signal on the remote host."""
+
+        command = 'kill -s %s %d' % (sig, self.pid)
+
+        # FIXME this keeps getting repeated
+        # pass this into bash -c 'command'
+        # command needs to be in "" quotes, with all the redirection outside
+        # eg. bash -c '"ls -la" < /dev/null >> out.txt'
+
+        command = '"%s" < /dev/null' % command
+
+        stdout, stderr, retcode = await self.execute(command)
+        self.log.debug("command: {} returned {} --- {} --- {}".format(command, stdout, stderr, retcode))
+        return (retcode == 0)
+
+    # FIXME clean up
     async def execute(self, command=None, stdin=None):
-        """
+        """Execute remote command via ssh.
+
         command: command to execute  (via bash -c command)
         stdin: script to pass in via stdin (via 'bash -s' < stdin)
-        executes command on remote system "command" and "stdin" are mutually exclusive
-        """
+        executes command on remote system "command" and "stdin" are mutually exclusive."""
 
         ssh_env = os.environ.copy()
 
@@ -195,187 +389,3 @@ class SSHSpawner(Spawner):
                 log_process(self, returncode, stdout, stderr)
                 
         return (stdout, stderr, returncode)
-    
-    def user_env(self):
-
-        env = super(SSHSpawner, self).get_env()
-        env.update(dict(
-            JPY_USER=self.user.name,
-            JPY_COOKIE_NAME=self.user.server.cookie_name,
-            JPY_BASE_URL=self.user.server.base_url,
-            JPY_HUB_PREFIX=self.hub.server.base_url,
-            JUPYTERHUB_PREFIX=self.hub.server.base_url,
-            # PATH=self.path
-            # NERSC local mod
-            PATH=self.path
-        ))
-
-        if self.notebook_dir:
-            env['NOTEBOOK_DIR'] = self.notebook_dir
-
-        hub_api_url = self.hub.api_url
-        if self.hub_api_url != '':
-            hub_api_url = self.hub_api_url
-
-        env['JPY_HUB_API_URL'] = hub_api_url
-        env['JUPYTERHUB_API_URL'] = hub_api_url
-
-        return env
-
-    async def exec_notebook(self, command):
-        env = self.user_env()
-        bash_script_str = "#!/bin/bash\n"
-
-        for item in env.items():
-            # item is a (key, value) tuple
-            # command = ('export %s=%s;' % item) + command
-            bash_script_str += 'export %s=%s\n' % item
-
-        # pass this into bash -c 'command'
-        # command needs to be in "" quotes, with all the redirection outside
-        # eg. bash -c '"ls -la" < /dev/null >> out.txt'
-        # We pass in /dev/null to stdin to avoid the hang
-        # Finally Grab the PID
-        # command = '"%s" < /dev/null >> jupyter.log 2>&1 & pid=$!; echo $pid' % command
-
-        bash_script_str += '%s < /dev/null >> jupyter.log 2>&1 & pid=$!\n' % command
-        bash_script_str += 'echo $pid\n'
-
-        run_script = "/tmp/{}_run.sh".format(self.user.name)
-        with open(run_script, "w") as f:
-            f.write(bash_script_str)
-        if not os.path.isfile(run_script):
-            raise Exception("The file " + run_script + "was not created.")
-        else:
-            with open(run_script, "r") as f:
-                self.log.debug(run_script + " was written as:\n" + f.read())
-
-        stdout, stderr, retcode = await self.execute(command, stdin=run_script)
-        self.log.debug("exec_notebook status={}".format(retcode))
-        if stdout != b'':
-            pid = int(stdout)
-        else:
-            return -1
-
-        return pid
-
-    async def remote_random_port(self):
-        # command = self.remote_port_command
-        # NERSC local mod
-        command = self.remote_port_command
-
-        # pass this into bash -c 'command'
-        # command needs to be in "" quotes, with all the redirection outside
-        # eg. bash -c '"ls -la" < /dev/null >> out.txt'
-        command = '"%s" < /dev/null' % command
-
-        stdout, stderr, retcode = await self.execute(command)
-
-        if stdout != b'':
-            port = int(stdout)
-        else:
-            self.log.error("could not get a remote port")
-            return None
-        self.log.debug("port={}".format(port))
-        return port
-
-    async def remote_signal(self, sig):
-        """
-        simple implementation of signal, which we can use
-        when we are using setuid (we are root)
-        """
-        command = 'kill -s %s %d' % (sig, self.pid)
-
-        # pass this into bash -c 'command'
-        # command needs to be in "" quotes, with all the redirection outside
-        # eg. bash -c '"ls -la" < /dev/null >> out.txt'
-        command = '"%s" < /dev/null' % command
-
-        stdout, stderr, retcode = await self.execute(command)
-        self.log.debug("command: {} returned {} --- {} --- {}".format(command, stdout, stderr, retcode))
-        return (retcode == 0)
-
-    async def start(self):
-        """Start the process"""
-        self.log.debug("Entering start")
-
-        port = await self.remote_random_port()
-        if port is None or port == 0:
-            return False
-        cmd = []
-
-        cmd.extend(self.cmd)
-        cmd.extend(self.get_args())
-
-        if self.hub_api_url != '':
-            old = '--hub-api-url=%s' % self.hub.api_url
-            new = '--hub-api-url=%s' % self.hub_api_url
-            for index, value in enumerate(cmd):
-                if value == old:
-                    cmd[index] = new
-        for index, value in enumerate(cmd):
-            if value[0:6] == '--port':
-                cmd[index] = '--port=%d' % (port)
-
-        remote_cmd = ' '.join(cmd)
-
-        # time.sleep(2)
-        # import pdb; pdb.set_trace()
-
-        self.pid = await self.exec_notebook(remote_cmd)
-
-        self.log.debug("Starting User: {}, PID: {}".format(self.user.name, self.pid))
-
-        if self.pid < 0:
-            return None
-        # DEPRECATION: Spawner.start should return a url or (ip, port) tuple in JupyterHub >= 0.9
-        return (self.remote_host, port)
-
-    async def poll(self):
-        self.log.debug("Entering poll")
-
-        if not self.pid:
-                # no pid, not running
-            self.clear_state()
-            return 0
-
-        # send signal 0 to check if PID exists
-        alive = await self.remote_signal(0)
-        self.log.debug("Polling returned {}".format(alive))
-
-        if not alive:
-            self.clear_state()
-            return 0
-        else:
-            return None
-
-    async def stop(self, now=False):
-        self.log.debug("Entering stop")
-
-        alive = await self.remote_signal(15)
-
-        self.clear_state()
-
-    def get_state(self):
-        """get the current state"""
-        self.log.debug("Entering get_state")
-
-        state = super().get_state()
-        if self.pid:
-            state['pid'] = self.pid
-        return state
-
-    def load_state(self, state):
-        """load state from the database"""
-        self.log.debug("Entering load_state")
-
-        super().load_state(state)
-        if 'pid' in state:
-            self.pid = state['pid']
-
-    def clear_state(self):
-        """clear any state (called after shutdown)"""
-        self.log.debug("Entering clear_state")
-        self.log.debug("Clearing User: {}, PID: {}".format(self.user.name, self.pid))
-        super().clear_state()
-        self.pid = 0
